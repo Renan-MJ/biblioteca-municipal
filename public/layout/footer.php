a8f9246973f4f4d0bee489bc1a6347eaee8a56fe
Renan Januário <RenanMachadoJanuario123@outlook.com>
2026-01-19 16:22:08 -0300

    menu drop mobile

--- public/layout/footer.php
+++ public/layout/footer.php
@@ -5,5 +5,10 @@
     <small>Biblioteca Municipal © <?= date('Y') ?></small>
 </footer>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
+
 </body>
 </html>
